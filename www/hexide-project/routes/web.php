950db92af58cc880915a17903f44cd96d37bbddd
Sergei Yurchenko <sergyurch@gmail.com>
2022-10-05 09:01:21 +0300

    Add authorization

--- www/hexide-project/routes/web.php
+++ www/hexide-project/routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return 'Hello world';
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
